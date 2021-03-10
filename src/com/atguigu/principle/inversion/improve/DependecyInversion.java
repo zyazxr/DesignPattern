a8d0a2b6e7d52ff9ea9e97979824e3abe0f6f58b
“zhaoyu4” <zhaoyu4@sgepri.sgcc.com.cn>
2021-03-10 11:19:14 +0800

    依赖倒转

--- src/com/atguigu/principle/inversion/improve/DependecyInversion.java
+++ src/com/atguigu/principle/inversion/improve/DependecyInversion.java
@@ -18,6 +18,7 @@ interface IReceiver {
 }
 
 class Email implements IReceiver {
+	@Override
 	public String getInfo() {
 		return "电子邮件信息: hello,world";
 	}
@@ -25,12 +26,14 @@ class Email implements IReceiver {
 
 //增加微信
 class WeiXin implements IReceiver {
+	@Override
 	public String getInfo() {
 		return "微信信息: hello,ok";
 	}
 }
 
 //方式2
+//客户端
 class Person {
 	//这里我们是对接口的依赖
 	public void receive(IReceiver receiver ) {
